5ccbf05c77a3c4af5316b0d15724155d56948d6c
alon.levy <alon.levy@infinitylabs.co.il>
2025-05-01 20:52:50 +0300

    init

---
+++ src/main/java/com/example/progo/ProgoApplication.java
@@ -0,0 +1,13 @@
+package com.example.progo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgoApplication.class, args);
+	}
+
+}
